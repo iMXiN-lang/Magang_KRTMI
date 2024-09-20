254a2765362a42ca0ae7c6f4c1d4f18cbc9e38bf
gdysidik <145112956+gdysidik@users.noreply.github.com>
2024-09-20 20:03:37 +0700

    Add files via upload

---
+++ Program_nema.ino
@@ -0,0 +1,30 @@
+// definisiin pin
+#define dir 48
+#define step 7
+#define enable 6
+
+void setup() {
+  pinMode(dir, OUTPUT);
+  pinMode(enable, OUTPUT);
+  pinMode(step, OUTPUT);
+
+  digitalWrite(enable, LOW); // enable on
+}
+
+void loop() {
+  digitalWrite(dir, LOW); // buat setel arahnya
+  for(int i = 0; i <= 4000; i++) {
+    digitalWrite(step, HIGH);
+    delayMicroseconds(200);
+    digitalWrite(step, LOW);
+    delayMicroseconds(200);
+  }
+  delay(500);
+  digitalWrite(dir, HIGH);
+  for(int i = 0; i <= 4000; i++) {
+    digitalWrite(step, HIGH);
+    delayMicroseconds(200);
+    digitalWrite(step, LOW);
+    delayMicroseconds(200);
+  }
+}
